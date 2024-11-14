7f2e43fa2a7aa20823ede932624c62ecccc4e381
Kyle Gerard Felker <felker@anl.gov>
2024-11-14 06:51:49 +0000

    Make it possible to just write XDMF and leave existing athdf

--- make_athdf_joblib.py
+++ make_athdf_joblib.py
@@ -13,9 +13,10 @@ import bin_convert_sc24 as bin_convert
 
 
 # Polaris compute node: 32 core CPU w/ HT
-nprocs = 16   # 32 = int(cpu_count())/2
-# 512 GB RAM
+nprocs = 24   # 32 = int(cpu_count())/2
+# 512 GB RAM per node
 # 4 procs ---> ~3.0 %CPU in top output
+# 32 workers grinds it to a hault
 
 # Main function
 def main(**kwargs):
@@ -34,17 +35,24 @@ def main(**kwargs):
 def convert_single_file(fname, verbose=False, overwrite=False):
     athdf_name = fname.replace(".bin", ".athdf")
     xdmf_name = athdf_name + ".xdmf"
-    # if only .athdf OR .athdf.xdmf exists, script will reconvert both in all cases
-    if (os.path.isfile(athdf_name) and os.path.isfile(xdmf_name)) and not overwrite:
+    athdf_exists = os.path.isfile(athdf_name)
+    xdmf_exists = os.path.isfile(xdmf_name)
+    if (athdf_exists and xdmf_exists) and not overwrite:
         if verbose:
-            print(f'Skipping source {fname} since overwrite={overwrite}')
+            print(f'Skipping source {fname} entirely since overwrite={overwrite}')
             print(f'and both {athdf_name} and {xdmf_name} already exist...')
     else:
-        filedata = bin_convert.read_binary(fname)
-        bin_convert.write_athdf(athdf_name, filedata)
-        bin_convert.write_xdmf_for(xdmf_name, os.path.basename(athdf_name), filedata)
         if verbose:
             print(f'Converting {fname}')
+        filedata = bin_convert.read_binary(fname)
+        if not athdf_exists or overwrite:
+            bin_convert.write_athdf(athdf_name, filedata)
+            if verbose:
+                print(f'Writing {athdf_name}')
+        if not xdmf_exists or overwrite:
+            bin_convert.write_xdmf_for(xdmf_name, os.path.basename(athdf_name), filedata)
+            if verbose:
+                print(f'Writing {xdmf_name}')
 
 
 if __name__ == '__main__':
